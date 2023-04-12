8743daaafe72fc99cf72ec2239ffa56e02a2d7ff
Dylan <dylan.7mcdougall@gmail.com>
2023-04-12 16:18:10 -0700

    removed javascript from html file, now importing from js file

--- public/app.js
+++ public/app.js
@@ -1,9 +1,6 @@
 let map;
 
 function initMap() {
-
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+  let map = new google.maps.Map(document.getElementById('map'), {zoom: 3, center: { lat: 36.737, lng: 119.787 }});
+  let marker = new google.maps.Marker({position: { lat: 36.737, lng: 119.787 }, map: map})
 }
