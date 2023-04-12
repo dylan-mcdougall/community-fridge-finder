bc9f5927d9c37a1c715077f68e0903e628240a12
Dylan <dylan.7mcdougall@gmail.com>
2023-04-12 11:27:18 -0700

    cleaned up working tree, server creation

---
+++ public/app.js
@@ -0,0 +1,16 @@
+import {Loader} from "/@googlemaps/js-api-loader"
+import apiKey from "../secrets.js"
+
+const loader = new Loader({
+  apiKey: apiKey,
+  version: "weekly"
+});
+
+loader.load().then(async () => {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  const map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
