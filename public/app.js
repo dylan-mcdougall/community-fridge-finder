836176a4307ee41c1acd23fefa3216be84270778
Dylan <dylan.7mcdougall@gmail.com>
2023-04-12 12:26:14 -0700

    server still needs to be tweaked

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-import {Loader} from "/@googlemaps/js-api-loader"
-import apiKey from "../secrets.js"
+import {Loader} from "./node_modules/@googlemaps/js-api-loader"
+import apiKey from "./secrets.js"
 
 const loader = new Loader({
   apiKey: apiKey,
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Start the server on the specified port
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'landing.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
