5826c2809ae7b9481fae3827912fa24d57764831
Dylan <dylan.7mcdougall@gmail.com>
2023-04-12 17:01:36 -0700

    updated styling and map positioning

--- public/app.js
+++ public/app.js
@@ -3,4 +3,6 @@ let map;
 function initMap() {
   let map = new google.maps.Map(document.getElementById('map'), {zoom: 3, center: { lat: 36.737, lng: 119.787 }});
   let marker = new google.maps.Marker({position: { lat: 36.737, lng: 119.787 }, map: map})
+
+  console.log(Object.entries(map))
 }
